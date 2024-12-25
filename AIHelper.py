7b9238228d09708b752511f2e4de658789848355
GregTao <gregtaoo@outlook.com>
2024-12-25 16:55:05 +0800

    AI动画更新

--- AIHelper.py
+++ AIHelper.py
@@ -3,6 +3,15 @@ import threading
 from openai import OpenAI
 
 CLIENT = None
+MESSAGES = [{
+    'role': 'system',
+    'content': 'You are now a assistant of this game. You must reply within 50 words. '
+               'You should help players with their questions about the '
+               'game. Player should beat the zombies in the first WORLD,'
+               ' and enter the 2nd world to interact with those NPCs,'
+               ' and then go back to 1st world and go to the 3rd world to beat the boss.'
+}]
+EVENTS = []
 
 
 def init():
@@ -14,18 +23,18 @@ def init():
 
 
 def get_response(input_text, role='user'):
-    return 'loading' if CLIENT is None else CLIENT.chat.completions.create(messages=[
-        {
-            'role': 'system',
-            'content': 'You are now a assistant of this game. You must reply within 50 words. '
-                       'You should help players with their questions about the '
-                       'game. Player should beat the zombies '
-        },
-        {
-            'role': role,
-            'content': input_text
-        },
-    ], model='llama3.2', stream=True)
+    user = {
+        'role': role,
+        'content': input_text
+    }
+    event = {
+        'role': 'system',
+        'content': ';'.join(EVENTS)
+    }
+    ret = 'loading' if CLIENT is None else CLIENT.chat.completions.create(messages=MESSAGES + [event, user],
+                                                                           model='llama3.2', stream=True)
+    MESSAGES.append(user)
+    return ret
 
 
 thread = threading.Thread(target=init)

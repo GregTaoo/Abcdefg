56a494879be83e6f2a90dbc6c5f19f8c0e208145
GregTao <gregtaoo@outlook.com>
2024-12-25 17:07:53 +0800

    AI动画更新

--- Config.py
+++ Config.py
@@ -15,3 +15,10 @@ LARGE_FONT = None
 HUGE_FONT = None
 SOUNDS = {}
 CLOCKS = []
+
+AI_URL = 'http://10.15.88.73:5011/v1'
+AI_PROMPT = ('You are now a assistant of this game. You must reply within 50 words. '
+             'You should help players with their questions about the '
+             'game. Player should beat the zombies in the first WORLD,'
+             ' and enter the 2nd world to interact with those NPCs,'
+             ' and then go back to 1st world and go to the 3rd world to beat the boss.')

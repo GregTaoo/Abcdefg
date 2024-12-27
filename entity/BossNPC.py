ad46664804ea2188e8fb2f2555f9aba314f2d0d4
Zcc <zhouchch2024@shanghaitech.edu.cn>
2024-12-27 21:45:41 +0800

    Boss NPC

---
+++ entity/BossNPC.py
@@ -0,0 +1,9 @@
+import Block
+import Config
+import I18n
+from Dialog import Dialog
+from entity.NPC import TraderNPC, TradeOption, NPC
+from render import Renderer
+from ui.DialogUI import DialogUI
+from ui.TradeUI import TradeUI
+

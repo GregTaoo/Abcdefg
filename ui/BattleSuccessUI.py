b5076448576c25d34f3ddb0149979c6e3d15faf2
Svania <limx2024@shanghaitech.edu.cn>
2025-01-03 14:33:48 +0800

    change some codes and add some exegesis

--- ui/BattleSuccessUI.py
+++ ui/BattleSuccessUI.py
@@ -24,11 +24,16 @@ class BattleSuccessUI(UI):
 
     def render(self, screen: pygame.Surface):
         super().render(screen)
+        # 绘制击败敌人的提示文本
         txt_surface = Config.LARGE_FONT.render(I18n.text('beat_entity').format(self.name), True, (0, 255, 0))
         screen.blit(txt_surface, (Config.SCREEN_WIDTH // 2 - txt_surface.get_width() // 2,
-                                  Config.SCREEN_HEIGHT // 2 - 75))
+                                  Config.SCREEN_HEIGHT // 2 - 75))  # 将文本居中绘制在屏幕上
+
+        # 绘制获得金币的提示文本
         txt_surface = Config.FONT.render(I18n.text('obtained_coins').format(self.coins), True, (255, 255, 255))
         screen.blit(txt_surface, (Config.SCREEN_WIDTH // 2 - txt_surface.get_width() // 2,
-                                  Config.SCREEN_HEIGHT // 2 - 35))
+                                  Config.SCREEN_HEIGHT // 2 - 35))  # 将金币文本居中绘制在屏幕上
+
+        # 绘制金币图标
         screen.blit(Config.COIN_IMAGE, (Config.SCREEN_WIDTH // 2 - txt_surface.get_width() // 2 - 22,
-                                        Config.SCREEN_HEIGHT // 2 - 37))
+                                        Config.SCREEN_HEIGHT // 2 - 37))  # 将金币图标绘制在文本旁边

dccf6a77fcd19449f8db9e3ccc072448c97edb65
Svania <limx2024@shanghaitech.edu.cn>
2025-01-03 14:32:54 +0800

    change some codes and add some exegesis

--- Dialog.py
+++ Dialog.py
@@ -10,10 +10,11 @@ class Dialog:
         self.load()
 
     def load(self):
-        with open(self.filename, 'r') as f:
-            data = json.load(f)
-            self.dialogs = data['data']
-            self.current = self.dialogs[data['start']]
+        # 从 JSON 文件加载对话数据
+        with open(self.filename, 'r') as f:  # 打开对话文件
+            data = json.load(f)  # 读取文件中的数据
+            self.dialogs = data['data']  # 将 'data' 部分加载到 dialogs 字典中
+            self.current = self.dialogs[data['start']]  # 设置当前对话为起始对话
 
     def next(self, choice: int):
         nxt = self.current['player'][choice]['next']
